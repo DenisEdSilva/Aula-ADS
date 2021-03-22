aa32ce7592dba9a2de38d577b91521bcbfbc0b03
Denis Eduardo <31224937+DenisEdSilva@users.noreply.github.com>
2021-03-22 19:00:14 -0400

    Add files via upload

---
+++ Aula 3/Java/Index.js
@@ -0,0 +1,17 @@
+$(document).ready(
+    function (){
+        alert("O documento carregou!")
+
+    }
+
+
+)
+
+
+$("#bt_inicio").click(
+    function(){
+        alert("Opa, você clicou no botão inicio.");
+
+    }
+
+)
