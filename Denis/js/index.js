6d8161d80a079f0c32a84811034a842b4492fc65
Denis Eduardo <31224937+DenisEdSilva@users.noreply.github.com>
2021-05-03 21:52:34 -0400

    Add files via upload
    Aula Tabela Ajax

---
+++ Denis/js/index.js
@@ -0,0 +1,25 @@
+$(document).ready(
+    function(){
+        alert('carregou')
+    }
+)
+
+$.ajax({
+    type:'GET',
+    url:'http://192.168.10.85:9000/users',
+    success: function(data){
+        $.each(data, function(i, user){
+            $('#tab_01').append(
+               '<tr>'+
+               '<td>'+user.guuid+'</td>',
+               '<td>'+user.name+'</td>',
+               '<td>'+user.cpf+'</td>'+
+                '</tr>'
+            )
+        })
+        console.log(data);
+    },
+    error: function(data){
+        console.log(data);
+    }
+})
